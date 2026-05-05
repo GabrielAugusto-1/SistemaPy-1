eed85499fd1d9d7b221405e17fab72c71e170ef6
Gabriel Augusto Firmino dos Santos <gabrielaugus6@gmail.com>
2026-05-05 15:59:48 -0300

    Melhorando o schema, e acrescentrando validação de campos invalidos, e @ no email

--- schema/clienteSchema.py
+++ schema/clienteSchema.py
@@ -1,29 +1,119 @@
-from utils import limpar
+from .utilsSchema import limpar
 
 class ClienteCreateSchema:
-
+   
     def __init__(self, dados):
+        self.nome = dados.get("nome")
+        self.email = dados.get("email")
+        self.senha = dados.get("senha")
+       
         self.erros = []
 
-        self.nome = limpar(dados.get("nome"))
-        self.email = limpar(dados.get("email"))
-        self.senha = limpar(dados.get("senha"))
+        try:
+            self.nome = limpar(self.nome)
+            self.email = limpar(self.email)
+            self.senha = str(self.senha).strip() if self.senha is not None else None
+        except Exception as e:
+            self.erros.append(f"Erro ao converter tipos: {str(e)}")
 
-        self.validar()
 
-    def validar(self):
+        campos_validos = {"nome", "email", "senha"}
+        campos_recebidos = set(dados.keys())
 
-        if not self.nome:
-            self.erros.append("Nome é obrigatório")
+        campos_invalidos = campos_recebidos - campos_validos
 
-        if not self.email:
-            self.erros.append("Email é obrigatório")
+        if campos_invalidos:
+            self.erros.append(f"Campo invalidos: {list(campos_invalidos)}")
+        self.erros.extend(self.validarCliente())
+        
+    
+    def validarCliente(self):
+        erros = []
 
-        if not self.senha:
-            self.erros.append("Senha é obrigatória")
+        if not self.nome or not self.nome.strip():
+            erros.append("Campo e valores de nome obrigatorio")
+            
+        if not self.email or not self.email.strip():
+            erros.append("Campo e valores de email obrigatorio")
 
-    def valido(self):
+        if self.email:
+            if not "@" in self.email:
+                erros.append("Email invalido! Verique se há @")
+    
+        if not self.senha or not self.senha.strip():
+            erros.append("Campo e valores de senha obrigatorio")
+        
+
+
+        return erros
+    
+    def eh_valido(self):
         return len(self.erros) == 0
+
+
+
+
+
+
+
+
+# class ClienteCreateSchema:
+
+#     def __init__(self, dados):
+#         self.erros = []
+
+#         self.nome = dados.get("nome")
+#         self.email = dados.get("email")
+#         self.senha = dados.get("senha")
+
+#         self.validar()
+
+#     def validar(self):
+
+#         if not self.nome:
+#             self.erros.append("Nome é obrigatório")
+
+#         if not self.email:
+#             self.erros.append("Email é obrigatório")
+
+#         if not self.senha:
+#             self.erros.append("Senha é obrigatória")
+
+#     def valido(self):
+#         return len(self.erros) == 0
+
+
+
+
+
+
+
+# from utils import limpar
+
+# class ClienteCreateSchema:
+
+#     def __init__(self, dados):
+#         self.erros = []
+
+#         self.nome = limpar(dados.get("nome"))
+#         self.email = limpar(dados.get("email"))
+#         self.senha = limpar(dados.get("senha"))
+
+#         self.validar()
+
+#     def validar(self):
+
+#         if not self.nome:
+#             self.erros.append("Nome é obrigatório")
+
+#         if not self.email:
+#             self.erros.append("Email é obrigatório")
+
+#         if not self.senha:
+#             self.erros.append("Senha é obrigatória")
+
+#     def valido(self):
+#         return len(self.erros) == 0
 
 
 
