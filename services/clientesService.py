0387b72bb70e4b7d59871585eef062315523b27b
Gabriel Augusto Firmino dos Santos <gabrielaugus6@gmail.com>
2026-05-05 22:10:19 -0300

    Colocando o metodo para atualizarCompleto, e o de buscar por id

--- services/clientesService.py
+++ services/clientesService.py
@@ -13,7 +13,7 @@ class ClienteService:
     
     @staticmethod
     def listar_cliente_todos():
-        dbcursor.execute("SELECT id, nome, email, situacao FROM Cliente" )
+        dbcursor.execute("SELECT id, nome, email, situacao FROM Clientes" )
         rows = dbcursor.fetchall()
 
         return [Cliente.peloDB(row).virarJSON() for row in rows]
@@ -21,8 +21,11 @@ class ClienteService:
 
     @staticmethod
     def listar_cliente_por_id(id):
-        dbcursor.execute("select id nome, email, situacao from Cliente where id=%s",id )
+        dbcursor.execute("select id, nome, email, situacao from Clientes where id=%s",(id,) )
         linha = dbcursor.fetchone()
+        if linha:
+            return Cliente.peloDB(linha).virarJSON()
+        return None
 
 
     
@@ -45,3 +48,7 @@ insert into clientes (nome, email, senha, situacao) values (%s, %s, %s, %s) retu
         return cliente.virarJSON()
 
 
+    @staticmethod
+    def atualizarClienteCompleto(id, dados):
+        dbcursor.execute("update set nome=%s, email=%s, senha=%s where id =%s", (dados["nome"],dados["email"],dados["senha"], id))
+        
